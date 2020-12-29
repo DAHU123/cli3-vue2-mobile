327fb2700f381a597b007d1ad3c0fb31f377218c
Chenlili <986220912@qq.com>
2020-12-29 18:32:12 +0800

    初始化项目的配置

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="handleClick">点击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  methods: {
+    handleClick() {
+      const params = {
+        username: "xupengpeng",
+        password: "7k5458"
+      };
+      // 进行接口请求
+      this.$api.postAccountLogin({ params }).then(res => {
+        console.log(res);
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import example from "@/store/modules/example";
+import getters from "@/store/getters";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    example
+  },
+  getters
+});
+
+export default store;
